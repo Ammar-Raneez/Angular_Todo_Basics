da166722fd44ee7c7fb4d972ea08a679e10fbc6d
Ammar <ammarraneez@gmail.com>
2020-11-27 21:16:41 +0530

    working with multiple components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from 'src/app-routing.module';
-import { TodoComponent } from './components/todo-component/todo/todo.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemsComponent } from './components/todo-items/todo-items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodosComponent,
+    TodoItemsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/todo-items/todo-items.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todos';
+
+@Component({
+  selector: 'app-todo-items',
+  templateUrl: './todo-items.component.html',
+  styleUrls: ['./todo-items.component.css']
+})
+export class TodoItemsComponent implements OnInit {
+  @Input() todo: Todo;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
